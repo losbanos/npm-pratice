7e851a6b76d8788dd73c12a3dc5a3bc9b35bdfc3
kimtaehoon <kimtaehoon@smilegate.com>
2024-01-10 08:58:31 +0900

    dotenv 사용

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import {Logger} from './logger';
-
+import 'dotenv/config';
+// import path from 'path';
+// dotenv.config({path: path.resolve('/.env')});
+console.log('secret = ', process.env.secret);
 export {
     Logger
 }
